7a55fc85c6a39d3fe0f928c82f3ab093c1cbec52
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-21 15:46:50 -0400

    lab complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create([{coupon_code: "10% Value" , store: "Philly"}, {coupon_code: "Stock Be Gone", store: "San Fransisco"}, {coupon_code: "Winter Half Off", store: "Nashville"}])
